a5a3f2254bee0fe318a6a162c339d87563fe6475
rafaapcode <rafaap2003@gmail.com>
2022-06-06 14:59:49 -0300

    Anotação REDUCE

--- Map, Filter e Reduce/reduce.js
+++ Map, Filter e Reduce/reduce.js
@@ -27,3 +27,51 @@ function callback(acc, value) {
 const precos = [10, 100.90, 120, 6, 78.50, 7.50];
 
 console.log(calcSaldo(precos, 323));
+
+
+
+const lista = [
+
+    {
+        chuteira: "Nike",
+        preco: 520
+    },
+    {
+        chuteira: "Adidas",
+        preco: 520
+    },
+
+    {
+        chuteira: "Mizuno",
+        preco: 340
+    },
+
+    {
+        chuteira: "Umbro",
+        preco: 200
+    },
+    {
+        chuteira: "Dray",
+        preco: 60
+    },
+    {
+        chuteira: "Puma",
+        preco: 270
+    },
+
+]
+
+function restoSaldo(arr, saldoDisponivel) {
+
+    // Ele vai pegar cada elemento do nosso ARRAY, por isso uso arr
+    return arr.reduce((prev, current) => {
+
+        // Current sera os nosso OBJs, por isso precisamos selecionar o preco dos nosso OBJs;
+        // Prev vai ser o saldoDisponivel, ou seja o valor que eu passar
+        return prev - current.preco;
+
+    }, saldoDisponivel);
+
+}
+
+console.log(restoSaldo(lista, 3500));
